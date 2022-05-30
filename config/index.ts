5e83961a1df9e3404f154b18774113910984b123
Achille MBOUGUENG <achille.mbougueng@yahoo.fr>
2022-05-30 05:59:26 +0200

    Build app zeeven

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://zeeven.com';
-export const backend = dev ? 'http://localhost:8087' : 'https://api.zeeeven.com';
+export const server = dev ? 'http://localhost:3000' : 'https://app.zeeven.chillo.fr';
+export const backend = dev ? 'http://localhost:8087' : 'https://api.zeeven.chillo.fr';
