f834891af5ea120f65e522609110bb2bdb1ee42f
Achille MBOUGUENG <achille.mbougueng@yahoo.fr>
2022-06-05 19:38:10 +0200

    build app

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -4,8 +4,28 @@ import { ClientSafeProvider, getCsrfToken, getProviders } from "next-auth/react"
 
 import Credentials from '../../components/providers/credentials';
 import { GetServerSideProps } from 'next';
+import axios from 'axios';
 
-function SignIn({ providers }: {providers: ClientSafeProvider[]}) {
+function SignIn({ providers : bee }: {providers: ClientSafeProvider[]}) {
+  const [providers, setProviders] = React.useState([]);
+  const readProviders = React.useCallback(async () => {
+      try {
+        const {data} = await axios.get(`${process.env.NEXTAUTH_URL}/api/auth/providers`);
+        console.log('====================================');
+        console.log({data});
+        if(data) {
+          setProviders(data);
+        }
+        console.log('====================================');
+      } catch (error) {
+        console.log('============error==================');
+        console.log({error});
+        console.log('============error==================');
+      }
+  }, [])
+  React.useEffect(() => {
+      readProviders();
+  }, [readProviders])
  console.log('====================================');
  console.log(providers);
  console.log('====================================');
