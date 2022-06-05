7246b9e6e2665fed9b45329f36cd49de0cec997c
Achille MBOUGUENG <achille.mbougueng@yahoo.fr>
2022-06-05 19:52:50 +0200

    build app

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -4,31 +4,9 @@ import { ClientSafeProvider, getCsrfToken, getProviders } from "next-auth/react"
 
 import Credentials from '../../components/providers/credentials';
 import { GetServerSideProps } from 'next';
-import axios from 'axios';
 
-function SignIn({ providers : bee }: {providers: ClientSafeProvider[]}) {
-  const [providers, setProviders] = React.useState([]);
-  const readProviders = React.useCallback(async () => {
-      try {
-        const {data} = await axios.get(`${process.env.NEXTAUTH_URL}/api/auth/providers`);
-        console.log('====================================');
-        console.log({data});
-        if(data) {
-          setProviders(data);
-        }
-        console.log('====================================');
-      } catch (error) {
-        console.log('============error==================');
-        console.log({error});
-        console.log('============error==================');
-      }
-  }, [])
-  React.useEffect(() => {
-      readProviders();
-  }, [readProviders])
- console.log('====================================');
- console.log(providers);
- console.log('====================================');
+function SignIn({ providers = []}: {providers: ClientSafeProvider[]}) {
+ 
 	return (
 		<OpenedStack>
       <>
@@ -52,6 +30,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const providers = await getProviders();
 
   return {
-    props: { providers },
+    props: { csrfToken,providers },
   }
 }
